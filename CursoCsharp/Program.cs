b406a80daa109785f9441703539393fb86f00625
Joan P. Alvarado <joansyn.s@gmail.com>
2019-08-15 11:27:11 -0500

    Update Program.cs

--- CursoCsharp/Program.cs
+++ CursoCsharp/Program.cs
@@ -1,15 +1,25 @@
-﻿using System;
+﻿//#Using Namespaces
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Namespace
 namespace CursoCsharp
+//Namespace's code block
 {
+    //Class
     class Program
     {
+        //Main Function
         static void Main(string[] args)
         {
+            //Vars
+
+            //Code
+            Console.Write("Hello World");
+            Console.ReadKey();
         }
     }
 }

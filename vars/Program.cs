e1a699d5dc9a52a225c72acbf9e4aed834c349b8
Joan P. Alvarado <joansyn.s@gmail.com>
2019-08-22 10:17:42 -0500

    Update Program.cs

--- vars/Program.cs
+++ vars/Program.cs
@@ -13,18 +13,18 @@ namespace vars
             // Variables 
             int i = 0;
             decimal x = 0.0m;
-            float y = 0.0f;
-            double z = 0.0D;
+            float f = 0.0f;
+            double d = 0.0D;
             string cadena = "Hola Mundo";
             bool flag = false;
             DateTime date = DateTime.MinValue;
 
             Console.WriteLine("El valor de 'i' es : {0}", i);
             Console.WriteLine("El valor de 'x' es : {0:C}", x);
-            Console.WriteLine("El valor de 'y' es : {0}", i);
-            Console.WriteLine("El valor de 'z' es : {0}", i);
-            Console.WriteLine("El valor de 'cadena' es : {0}", i);
-            Console.WriteLine("El valor de 'flag' es : {0}", i);
+            Console.WriteLine("El valor de 'f' es : {0:F2}", f);
+            Console.WriteLine("El valor de 'd' es : {0:F2}", d);
+            Console.WriteLine("El valor de 'cadena' es : " +cadena);
+            Console.WriteLine("El valor de 'flag' es : " + flag.ToString());
             Console.WriteLine("El valor de 'date' es : {0}", i);
             Console.ReadKey();
 

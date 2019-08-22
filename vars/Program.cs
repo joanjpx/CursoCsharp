60f44127191e4e3749ec5169fb6c564b8c369880
Joan P. Alvarado <joansyn.s@gmail.com>
2019-08-22 10:14:27 -0500

    Variables en C#

---
+++ vars/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables 
+            int i = 0;
+            decimal x = 0.0m;
+            float y = 0.0f;
+            double z = 0.0D;
+            string cadena = "Hola Mundo";
+            bool flag = false;
+            DateTime date = DateTime.MinValue;
+
+            Console.WriteLine("El valor de 'i' es : {0}", i);
+            Console.WriteLine("El valor de 'x' es : {0:C}", x);
+            Console.WriteLine("El valor de 'y' es : {0}", i);
+            Console.WriteLine("El valor de 'z' es : {0}", i);
+            Console.WriteLine("El valor de 'cadena' es : {0}", i);
+            Console.WriteLine("El valor de 'flag' es : {0}", i);
+            Console.WriteLine("El valor de 'date' es : {0}", i);
+            Console.ReadKey();
+
+        }
+    }
+}
